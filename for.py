0f7e89df7d6eb44407804885fbc2610d71f722f2
Antoniozus89 <166949414+Antoniozus89@users.noreply.github.com>
2024-04-23 17:55:19 +0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,7 @@
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in range(len(list_)):
+    print('Я езжу на машине ', list_[i])
+cars_count = 0
+for cars in list_:
+        cars_count += 10
+print('Всего машин', cars_count )
